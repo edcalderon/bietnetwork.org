1bb43d90f743619715f82e3bd6f2f2740e616594
ed <edwardca12@hotmail.com>
2025-11-18 18:12:22 -0500

    chore: bump version to 0.2.13

--- apps/web/docs/static/chunks/webpack.js
+++ apps/web/docs/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "40124963b0d36538"; }
+/******/ 		__webpack_require__.h = function() { return "da849534fc8b25c1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
